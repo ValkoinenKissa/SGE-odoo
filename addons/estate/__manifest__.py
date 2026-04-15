07a1f13f4c14573ba5a95f3f8f19ce5ab11a3e84
Alberto Bollo <albertobollo7@protonmail.com>
2026-04-15 06:06:11 +0200

    feat(estate): add new estate module with models, views, and data

---
+++ addons/estate/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Real Estate",
+    "summary": "Real Estate",
+    "author": "Ronny Montano",
+    "license": "AGPL-3",
+    "website": "https://github.com/curonny/curso_odoo_18",
+    "category": "Real Estate",
+    "version": "18.0.0.0.1",
+    "depends": ["base", "mail"],
+    "data": [
+        "data/estate_category_data.xml",
+        "views/estate_view.xml",
+        "security/ir.model.access.csv",
+    ],
+    "demo": [],
+}
